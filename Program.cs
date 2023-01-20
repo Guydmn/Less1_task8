73de4ac1c12aad5f0441bbc58350fe8237769276
Roman Guydmn <romanguydmn@gmail.com>
2023-01-20 17:55:21 +0400

    Задача 8

---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+Console.Write("Введите число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+x = a % 2;
+if (x = 0)
+Console.Write()
