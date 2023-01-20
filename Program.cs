8e7ed3231de51dfbd3df48ce19f5262e88d2dde7
Roman Guydmn <romanguydmn@gmail.com>
2023-01-20 19:16:33 +0400

    Задача 8

--- Program.cs
+++ Program.cs
@@ -3,6 +3,15 @@ Console.WriteLine("Hello, World!");
 
 Console.Write("Введите число: ");
 int a = Convert.ToInt32(Console.ReadLine());
-x = a % 2;
-if (x = 0)
-Console.Write()
+int count = 1;
+while (count <= a) 
+{
+int x = count % 2;
+   {
+    if (x == 0)
+       {
+        Console.Write(count);
+       } 
+   }
+ count = count + 1;
+}
